14d02e98468b772543f34f4c7070faeb77457358
kretika arora <kretikaarora@gmail.com>
2020-10-06 19:06:19 +0530

    first name validation

--- Program.cs
+++ Program.cs
@@ -7,10 +7,9 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to User Registration");
-            Console.WriteLine("please enter your name");
-            string firstName = Console.ReadLine();
-            UserValidation userValidation = new UserValidation();
-            userValidation.CheckNameValidation(firstName);
+            UsersDetails usersDetails = new UsersDetails();
+            usersDetails.FirstNameDetail();
+
 
 
 
