fe7b6f2176d817c60af4500e211b36eb7286e8c0
kretika arora <kretikaarora@gmail.com>
2020-10-13 13:20:00 +0530

    [Kretika Arora] User Validation

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Text.RegularExpressions;
-using System.Transactions;
-using System.Xml;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Kretika Arora"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace UserRegistration
 {
-    class Program
+    using System;
+
+    /// <summary>
+    /// entry point for Regex program
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
         {
             Console.WriteLine(" welcome to User Registration");
             Console.WriteLine("1. Press 1 for validation of  first name");
@@ -18,12 +28,12 @@ namespace UserRegistration
             Console.WriteLine("6. Press 6 to exit");
 
             UsersDetails usersDetails = new UsersDetails();
-            
+
             bool flag = true;
             while (flag)
             {
                 Console.WriteLine("Please select the number 1. first name  2. last name 3. phone number 4. email  5. password 6. Exit ");
-                
+
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -48,32 +58,8 @@ namespace UserRegistration
                     default:
                         Console.WriteLine("please enter a valid number");
                         break;
-
-
-
-
-
-
                 }
             }
-
-            
-            
-           
-            
-            
-
-
-
-
-
-
-
-
-
-
-
-
         }
     }
-}
+ }
--- UserDetails.cs
+++ UserDetails.cs
@@ -19,14 +19,16 @@ namespace UserRegistration
                 bool check = userValidation.CheckNameValidation(firstName);
                 if (check == true)
                 {
-                    Console.WriteLine("the name entered is valid");
+                    Console.WriteLine("the name {0} is valid",firstName);
+                    Console.WriteLine("successful entry");
                     break;
 
                 }
                 else
                 {
-                    Console.WriteLine("the name entered is not valid");
-                    Console.WriteLine("enter a valid name ! ");
+                    Console.WriteLine("unsuccessful entry");
+                    Console.WriteLine("the name {0} is not valid",firstName);
+                    Console.WriteLine("enter a valid name with min three alphabets and first letter capital ");
                 }
 
             }
@@ -43,14 +45,16 @@ namespace UserRegistration
                 bool check = userValidation.CheckNameValidation(lastName);
                 if (check == true)
                 {
-                    Console.WriteLine("the name entered is valid");
+                    Console.WriteLine("the name {0} is valid",lastName);
+                    Console.WriteLine("successful entry");
                     break;
 
                 }
                 else
                 {
-                    Console.WriteLine("the name entered is not valid");
-                    Console.WriteLine("enter a valid name ! ");
+                    Console.WriteLine("unsuccessful entry");
+                    Console.WriteLine("the name {0} is not valid",lastName);
+                    Console.WriteLine("enter a valid name with min three alphabets and first letter capital ");
                 }
 
             }
@@ -67,14 +71,16 @@ namespace UserRegistration
                 bool check = userValidation.CheckEmailValidation(email);
                 if (check == true)
                 {
-                    Console.WriteLine("the email entered is valid");
+                    Console.WriteLine("the email id {0} is valid",email);
+                    Console.WriteLine("successful entry");
                     break;
 
                 }
                 else
                 {
-                    Console.WriteLine("the email entered is not valid");
-                    Console.WriteLine("enter a valid email ! ");
+                    Console.WriteLine("unsuccessful entry");
+                    Console.WriteLine("the email id {0} is not valid",email);
+                    Console.WriteLine(" enter an email id with proper format");
                 }
 
             }
@@ -92,13 +98,15 @@ namespace UserRegistration
                 if (check == true)
                 {
                     Console.WriteLine("the phone number  entered is valid");
+                    Console.WriteLine("successful entry");
                     break;
 
                 }
                 else
                 {
+                    Console.WriteLine("unsuccessful entry");
                     Console.WriteLine("the phone number entered is not valid");
-                    Console.WriteLine("enter a valid phone number ! ");
+                    Console.WriteLine("enter a  phone number with a gap in between 10 digit number and pincode ");
                 }
 
             }
@@ -115,14 +123,17 @@ namespace UserRegistration
                 bool check = userValidation.CheckPassword(password);
                 if (check == true)
                 {
-                    Console.WriteLine("the password entered is valid");
+                    Console.WriteLine("the password {0} is valid",password);
+                    Console.WriteLine("successful entry");
                     break;
 
                 }
                 else
                 {
+                    Console.WriteLine("unsuccessful entry");
                     Console.WriteLine("the password entered is not valid");
-                    Console.WriteLine("enter a valid password  ! ");
+                    Console.WriteLine("enter a valid password with \n 1.atleast one capital letter \n 2.atleast one small letter");
+                    Console.WriteLine(" 3.one special character \n 4.atleast one digit");
                 }
 
             }
